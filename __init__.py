004313d354a35cdfc85db33bd84a6c5c1a594241
romeoabulencia <romeo.abulencia@gmail.com>
2014-08-22 08:54:19 +0800

    Patched sales order error 1.1

--- __init__.py
+++ __init__.py
@@ -1,1 +1,3 @@
 import report
+
+import sale_order
---
+++ sale_order.py
@@ -0,0 +1,24 @@
+
+
+from openerp.osv import fields, osv
+from openerp import netsvc
+
+
+
+class sale_order(osv.osv):
+    _name = "sale.order"
+    _inherit = "sale.order"
+
+    def print_quotation(self, cr, uid, ids, context=None):
+        '''
+        This function prints the sales order and mark it as sent, so that we can see more easily the next step of the workflow
+        '''
+        assert len(ids) == 1, 'This option should only be used for a single id at a time'
+        wf_service = netsvc.LocalService("workflow")
+        wf_service.trg_validate(uid, 'sale.order', ids[0], 'quotation_sent', cr)
+        datas = {
+                 'model': 'sale.order',
+                 'ids': ids,
+                 'form': self.read(cr, uid, ids[0], context=context),
+        }
+        return {'type': 'ir.actions.report.xml', 'report_name': 'sale.order.inherit', 'datas': datas, 'nodestroy': True}
